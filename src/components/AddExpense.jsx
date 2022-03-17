67580825a0f964301f55371e763a544af1bef995
shyam500 <shyamppk2000@gmail.com>
2022-03-17 20:17:32 +0530

    Created notification component

--- src/components/AddExpense.jsx
+++ src/components/AddExpense.jsx
@@ -3,24 +3,40 @@ import { useContext } from "react";
 import classes from "./AddExpense.module.css";
 import context from "./store/context";
 
-
 const AddExpense = () => {
-    const{items,total} = useContext(context);
-    const [expense,setExpense]=useState('');
-    
-    const formSubmitHandler=(e)=>{
-        e.preventDefault();
+  const { items, total } = useContext(context);
+  const [expense, setExpense] = useState("");
+  const [price, setPrice] = useState(0);
+
+  const expenseHandler = (e) => {
+    setExpense(e.target.value);
+  };
+
+  const priceHandler = (e) => {
+    setPrice(e.target.value);
+  };
+
+  const formSubmitHandler = (e) => {
+    e.preventDefault();
+    if(expense.length===0&&price<1){
+        
     }
+  };
 
   return (
-      
     <form className={classes.expense_container} onSubmit={formSubmitHandler}>
       <input
+        onChange={() => {
+          expenseHandler;
+        }}
         type="text"
         placeholder="Enter Expense "
         className={classes.input}
       />
       <input
+        onChange={() => {
+          priceHandler;
+        }}
         type="number"
         placeholder="Enter Price"
         className={classes.input}
---
+++ src/components/UI/Notification.jsx
@@ -0,0 +1,16 @@
+import classes from "./Notification.module.css";
+
+const Notification = ({ message, func }) => {
+  const buttonHandler = () => {
+      func();
+  };
+
+  return (
+    <section className={classes.container}>
+      <h2>{message}</h2>
+      <button onClick={buttonHandler}>OK</button>
+    </section>
+  );
+};
+
+export default Notification;
