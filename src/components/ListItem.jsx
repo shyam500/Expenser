db4dec80de6b046bebf869ef2f7a5e6bb1edad4c
shyam500 <shyamppk2000@gmail.com>
2022-03-17 21:48:43 +0530

    created ListItem component

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -1,11 +1,11 @@
 import classes from "./ListItem.module.css";
-
+   
 const ListItem = ({ id, item, price }) => {
   return (
     <li key={id} className={classes.list_item}>
       {item}
       <span className={classes.list_price}>{price} rupees</span>
-      <span className={classes.delete_btn}>X</span>
+      <span className={classes.delete_btn}> X </span>
     </li>
   );
 };
